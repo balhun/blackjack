6aaf6ff43784398a2be58c8f6fb53ce41601a92a
hunor000 <hunorgaming000@gmail.com>
2024-04-21 00:20:31 +0200

    ordenare nagy faszsag FIX FIX

--- src/main/java/com/example/blackjack/ServerController.java
+++ src/main/java/com/example/blackjack/ServerController.java
@@ -35,13 +35,13 @@ public class ServerController {
 
     //Arrays
     public String[] initialDeck = {
-            "2H", "3H", "4H", "5H", "6H", "7H", "8H", "9H", "JH", "QH", "KH", "AH", //Szív
-            "2S", "3S", "4S", "5S", "6S", "7S", "8S", "9S", "JS", "QS", "KS", "AS", //Pikk
-            "2C", "3C", "4C", "5C", "6C", "7C", "8C", "9C", "JC", "QC", "KC", "AC", //Treff
-            "2D", "3D", "4D", "5D", "6D", "7D", "8D", "9D", "JD", "QD", "KD", "AD"  //Káró
+            "2H", "3H", "4H", "5H", "6H", "7H", "8H", "9H", "10H", "JH", "QH", "KH", "AH", //Szív
+            "2S", "3S", "4S", "5S", "6S", "7S", "8S", "9S", "10S", "JS", "QS", "KS", "AS", //Pikk
+            "2C", "3C", "4C", "5C", "6C", "7C", "8C", "9C", "10C", "JC", "QC", "KC", "AC", //Treff
+            "2D", "3D", "4D", "5D", "6D", "7D", "8D", "9D", "10D", "JD", "QD", "KD", "AD"  //Káró
     };
 
-    public int[] initialDeckValue = { 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10 };
+    public int[] initialDeckValue = { 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10, 10 };
 
     public LinkedList<String> mainDecks = new LinkedList<>();
     public LinkedList<Integer> mainDecksValue = new LinkedList<>();
@@ -83,7 +83,9 @@ public class ServerController {
 
     //Fills the main deck array
     public void fillMainDecksValues() {
-        for (int i = 0; i < 6; i++) for (int j = 0; j < initialDeckValue.length; j++) mainDecksValue.add(initialDeckValue[j]);
+        for (int i = 0; i < 24; i++) for (int j = 0; j < initialDeckValue.length; j++) mainDecksValue.add(initialDeckValue[j]);
+        System.out.println("mainDecksValue.size() = " + mainDecksValue.size());
+        System.out.println("mainDecks.size() = " + mainDecks.size());
     }
 
     //Start round button
